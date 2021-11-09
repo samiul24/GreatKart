dce95ae3a1c4cf7ff1e95ad7f4ed18c7a4a2c6db
samiul24 <samiul110624@gmail.com>
2021-11-09 08:02:39 +0600

    09_11_2021

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from carts.models import CartItem, Cart
+from store.models import Product
 
 def cart(request):
     return render(request, 'store/cart.html')
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+            )
+    cart.save()
+
