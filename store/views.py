4c08390c461906a6888a42a92647b5f411c35a12
samiul24 <samiul110624@gmail.com>
2021-11-27 22:36:12 +0600

    27_11_2021

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from .models import Product
 from store.models import Category
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+
 from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -23,14 +27,23 @@ def store(request, category_slug=None):
 
     return render(request, 'store/store.html', context)
 
-def product_detail(request,category_slug, product_slug):
+def product_detail(request, category_slug, product_slug):
+    #print(category_slug)
+    #print(product_slug)
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        #in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product.id)
+        #print(in_cart.query) /*Without exists() it works*/
+        #print(in_cart)
+        #print(single_product)
+        #print(single_product.id)
     except Exception as e:
         raise e 
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
