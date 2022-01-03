4f97d59be7c83fb423b06be077b29b939747ca32
samiul24 <samiul110624@gmail.com>
2022-01-04 00:09:31 +0600

    04_01_2021

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.home, name='home' ),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
