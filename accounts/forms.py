a7298d8b441d964955d2bdb77465eddd5fa3cd84
samiul24 <samiul110624@gmail.com>
2022-01-10 08:05:38 +0600

    10_01_2021

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,8 @@ from django.db import models
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField()
+    confirm_password = forms.CharField()
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
